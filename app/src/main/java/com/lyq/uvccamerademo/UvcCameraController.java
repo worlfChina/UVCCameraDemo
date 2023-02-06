2b1f13fd9c1ad1914e823ebb0d4f12395afca05b
Destiny <32727597+1369521908@users.noreply.github.com>
2023-02-06 10:32:18 +0800

    Update UvcCameraController.java
    fixed list reset

--- app/src/main/java/com/lyq/uvccamerademo/UvcCameraController.java
+++ app/src/main/java/com/lyq/uvccamerademo/UvcCameraController.java
@@ -48,6 +48,8 @@ public class UvcCameraController {
 
     public void init(MainActivity activity) {
         this.mWeakReference = new WeakReference(activity);
+        mCameraViewInterface.clear();
+        mUvcCameraHandlerList.clear();
         if (null != mWeakReference) {
             MainActivity mainActivity = mWeakReference.get();
             if (null != mainActivity) {
